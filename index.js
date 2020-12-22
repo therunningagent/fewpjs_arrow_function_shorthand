ffff232f49bf279591e8ea7a1073e7a38f26317c
therunningagent <luistjr@gmail.com>
2020-12-22 10:48:10 -0500

    completed

---
+++ index.js
@@ -0,0 +1,12 @@
+function divide() {
+    let answer = 2000 / 100
+
+    return answer
+}
+
+let square = x => x * x
+
+console.log(square(2))
+
+let add = (parameter1, parameter2) => parameter1 + parameter2
+console.log(add(5,5))
